7457b9b65727572309a26fa5e2830fc516dd331e
OscJD <oscar5-juarez@hotmail.com>
2017-11-09 12:23:01 -0600

    Update getTime.py

--- getTime.py
+++ getTime.py
@@ -2,23 +2,29 @@ from urllib import urlopen
 import os
 import time
 
-#res=urlopen('http://just-the-time.appspot.com/')
+resDate=urlopen('http://just-the-time.appspot.com/')
 
 while(True):
     try:
         strin=r'<span id="currentTime">'
-
         res=urlopen('http://24timezones.com/es_husohorario/mexico_city_hora_actual.php')
+        resDate=urlopen('http://just-the-time.appspot.com/')
 
         timeStr=res.read().strip()
+        dateStr=resDate.read().strip()
         particion=timeStr.split(strin)
-        print particion
+        particionDate=dateStr.split(" ")
+       
         timeAct=str(particion[1]).split(" ")
-        print "sudo date -s "+timeAct[0]+" "+timeAct[1][0:2]
+        dateAct=str(particionDate[0])
+        print dateAct
+        print "sudo date -s "+dateAct+" "+timeAct[0]+" "+timeAct[1][0:2]
 
-        os.system("sudo date -s "+timeAct[0]+timeAct[1][0:2])
+        os.system("sudo date -s "+dateAct+" "+timeAct[0]+" "+timeAct[1][0:2])
         timeStr=""
+        dateStr=""
         particion={}
+        particionDate{}
         time.sleep(60)
 
     except:
